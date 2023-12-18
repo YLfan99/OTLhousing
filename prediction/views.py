3a51206e7c1dc71498a888a6a1129cd756567bd8
YLfan99 <YLfanxuan@outlook.com>
2023-12-18 21:36:49 +1100

    new varibale input

--- prediction/views.py
+++ prediction/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-
+from .form import PredictForm
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
     if request.method == 'POST':
         # 获取前端输入数据
-        user_input = request.POST.get('user_input')
+        form = PredictForm(request.POST)
+        if form.is_valid():
+            # 在这里运行后端逻辑，处理 user_input 数据
+            X = process_input(form)
         
-        # 在这里运行后端逻辑，处理 user_input 数据
-        X = process_input(user_input)
         result = model.predict([X])
         return render(request, 'result.html', {'result': result[0]})
-    return render(request, 'index.html')
+    
+    context = {}
+    context['form'] = PredictForm()
+    return render(request, 'index.html', context)
 
+def process_input(input):
+    pass
